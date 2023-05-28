5cd9b87d578106cb15655ce832ca656891b63bbe
mateenshaik <mateenshaikh046@gmail.com>
2023-05-28 20:05:08 +0530

    groovy name change

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,10 +1,10 @@
 def call(Map stageParams) {
 
-   #checkout{[
-    #$class: 'GitSCM',
-     #branches: [[name: stageParams.branch ]],
-      #userRemoteConfigs: [[ url: stageParams.url ]]
+   checkout{[
+      $class: 'scmGit',
+      branches: [[name: stageParams.branch ]],
+      userRemoteConfigs: [[ url: stageParams.url ]]
 
-    checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/mateenshaik/Aws_project2.git']])    
+    //checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/mateenshaik/Aws_project2.git']])    
     ]}
 }
