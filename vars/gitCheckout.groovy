1046a17727184581ca2d1b2e1cd337a7b45f16f1
mateenshaik <mateenshaikh046@gmail.com>
2023-05-28 19:32:14 +0530

    groovy name change

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,7 +3,7 @@ def call(Map stageparams){
     checkout{[
         $class: 'GitSCM',
         branches: [{name: stageParams.branch }],
-        userRemoteConfigs: [{ url: stageparams.url }]
+        userRemoteConfigs: [{ url: stageParams.url }]
     ]
 
 
