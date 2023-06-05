22f72146d5b43a9084988176df97631272290a44
mateenshaik <mateenshaikh046@gmail.com>
2023-06-05 21:30:26 +0530

    add sonarqube code

--- vars/StaticCodeAnalysis.groovy
+++ vars/StaticCodeAnalysis.groovy
@@ -1,5 +1,6 @@
 def call(credentialsId){
-    withSonarQubeEnv(credentialsId: 'credentialsId') {
+
+    withSonarQubeEnv(credentialsId: credentialsId) {
     sh 'mvn clean package sonar:sonar'
 }
 }
